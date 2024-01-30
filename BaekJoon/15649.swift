9bf8944472952869667e6b205aa03d6b20e8496c
sikwan <yysskk99999@naver.com>
2024-01-30 16:42:09 +0900

    15649

---
+++ BaekJoon/15649.swift
@@ -0,0 +1,8 @@
+//
+//  15649.swift
+//  BaekJoon
+//
+//  Created by 양시관 on 1/30/24.
+//
+
+import Foundation
--- BaekJoon/main.swift
+++ BaekJoon/main.swift
@@ -10,22 +10,26 @@ import Foundation
 import SwiftUI
 import Foundation
 
-let str1 = ["0"] + readLine()!.map { $0 } // 0이라는 스트링 앞에 redaline으로 배열을 추가할수있도록 만들어줌 붙이는 이유는 인덱스를 0부터 시작할수 있게 하기위해서
-let str2 = ["1"] + readLine()!.map { $0 }
-let n = str1.count// str1 의 개수를 세줌
-let m = str2.count
+var nm = readLine()!.split(separator: " ").map{ Int($0)! }
+let N = nm[0]
+let M = nm[1]
 
-var cache = [[Int]](repeating: [Int](repeating: 0, count: n), count: m)
+var stack = [Int]()
 
-for y in 1..<m {
-    for x in 1..<n {
-        if str2[y] != str1[x] { //같은 위치의 값이 같은경우가 아닌경우에 이 소스블록을 따라감
-            cache[y][x] = max(cache[y - 1][x], cache[y][x - 1])// 이게 어떻게 되는지는 알고리즘을 그려봐야알듯,
-        } else {// 문자가 같은경우
-            cache[y][x] = cache[y - 1][x - 1] + 1//대각선에다가 + 1 을 해주면 됨
+private func dfs() {
+    if stack.count == M {
+        print(stack.map{ String($0) }.joined(separator:" "))
+        return
+    }
+
+    for i in 1...N {
+        if !stack.contains(i) {
+            stack.append(i)
+            dfs()
+            stack.removeLast()
         }
+
     }
 }
 
-print(cache[m - 1][n - 1])
-
+dfs()
