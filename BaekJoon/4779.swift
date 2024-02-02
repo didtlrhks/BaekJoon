cb3fc7fdc259fdb689e6a53be566e92c38b9f871
sikwan <yysskk99999@naver.com>
2024-02-02 18:36:38 +0900

    14888

--- BaekJoon/4779.swift
+++ BaekJoon/4779.swift
@@ -1,8 +1,27 @@
+////
+////  4779.swift
+////  BaekJoon
+////
+////  Created by 양시관 on 2/1/24.
+////
 //
-//  4779.swift
-//  BaekJoon
+//import Foundation
 //
-//  Created by 양시관 on 2/1/24.
 //
-
-import Foundation
+//func split(n : Double) -> String {
+//
+//    if n == 0 {
+//          return "-"
+//      }
+//
+//
+//
+//  let line = split(n: n - 1)
+//  return line + String(repeating: " ", count: Int(pow(3, n - 1))) + line
+//
+//}
+//
+//
+//while let input = readLine(), let n = Double(input) {
+//  print(split(n: n))
+//}
