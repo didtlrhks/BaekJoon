211e1e43b6ab65b442af872733008191960fe2f1
sikwan <yysskk99999@naver.com>
2024-02-05 21:36:50 +0900

    15651

---
+++ BaekJoon/14889.swift
@@ -0,0 +1,8 @@
+//
+//  14889.swift
+//  BaekJoon
+//
+//  Created by 양시관 on 2/5/24.
+//
+
+import Foundation
---
+++ BaekJoon/15652.swift
@@ -0,0 +1,8 @@
+//
+//  15652.swift
+//  BaekJoon
+//
+//  Created by 양시관 on 2/5/24.
+//
+
+import Foundation
