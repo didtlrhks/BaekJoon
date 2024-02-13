de087c73f39ba053130c6817bff7f592eac4e8b3
sikwan <yysskk99999@naver.com>
2024-02-13 21:16:15 +0900

    1912

---
+++ BaekJoon/1912.swift
@@ -0,0 +1,29 @@
+////
+////  1912.swift
+////  BaekJoon
+////
+////  Created by 양시관 on 2/13/24.
+////
+//
+//import Foundation
+////
+////  main.swift
+////  BaekJoon
+////
+////  Created by 양시관 on 1/19/24.
+//import Foundation
+//
+//let N = Int(String(readLine()!))!
+//let arr1 = readLine()!.split(separator: " ").map{
+//    Int(String($0))!
+//}
+//
+//var dp1 = Array(repeating: -9999, count: 100001)
+//
+//dp1[0] = arr1[0] // 이게 첫번째 요소가 가장 큰경우인거고
+//
+//for i in 1..<N {
+//    dp1[i] = max(arr1[i],arr1[i] + dp1[i-1])
+//}
+//print(dp1.max()!)
+//
--- BaekJoon/main.swift
+++ BaekJoon/main.swift
@@ -5,34 +5,17 @@
 //  Created by 양시관 on 1/19/24.
 import Foundation
 
-
-func solution() -> Int {
-    let n = Int(String(readLine()!))!
-    var stairs: [Int] = []
-    var value: [Int] = [Int](repeating: 0, count: n)
-    
-    for _ in 0..<n{
-        stairs.append(Int(String(readLine()!))!)
-    }
-
-    if n == 1 {
-        return stairs[0]
-    } else if n == 2{
-        return stairs[0] + stairs[1]
-    } else if n == 3 {
-        return max(stairs[0] + stairs[2], stairs[1] + stairs[2])
-    } else {
-        value[0] = stairs[0]
-        value[1] = max(stairs[0] + stairs[1], stairs[1])
-        value[2] = max(stairs[0] + stairs[2], stairs[1] + stairs[2])
-        
-        for i in 3..<stairs.count{
-            value[i] = max(stairs[i] + value[i-2], stairs[i] + stairs[i-1] + value[i-3])
-        }
-        
-        return value[value.count - 1]
-    }
+let N = Int(String(readLine()!))!
+let arr1 = readLine()!.split(separator: " ").map{
+    Int(String($0))!
 }
 
-print(solution())
-출처: https://icksw.tistory.com/115 [PinguiOS:티스토리]
+var dp1 = Array(repeating: -9999, count: 100001)
+
+dp1[0] = arr1[0] // 이게 첫번째 요소가 가장 큰경우인거고
+
+for i in 1..<N {
+    dp1[i] = max(arr1[i],arr1[i] + dp1[i-1])
+}
+print(dp1.max()!)
+
