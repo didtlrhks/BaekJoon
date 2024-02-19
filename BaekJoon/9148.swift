537f3c28b9a5ff1bd8f2a843177efa0a2c54937b
sikwan <yysskk99999@naver.com>
2024-02-19 22:53:35 +0900

    9148

---
+++ BaekJoon/9148.swift
@@ -0,0 +1,34 @@
+////
+////  9148.swift
+////  BaekJoon
+////
+////  Created by 양시관 on 2/19/24.
+////
+//
+//import Foundation
+//var w = [[[Int]]](repeating: [[Int]](repeating: [Int](repeating: 1, count: 21), count: 21), count: 21)
+//
+//for a in 1...20 {
+//    for b in 1...20 {
+//        for c in 1...20 {
+//            if a < b && b < c {
+//                w[a][b][c] = w[a][b][c - 1] + w[a][b - 1][c - 1] - w[a][b - 1][c]
+//            } else {
+//                w[a][b][c] = w[a - 1][b][c] + w[a - 1][b - 1][c] + w[a - 1][b][c - 1] - w[a - 1][b - 1][c - 1]
+//            }
+//        }
+//    }
+//}
+//
+//while let input = readLine()?.split(separator: " ").map({ Int($0)! }), input != [-1 ,-1, -1] {
+//    let a = input[0], b = input[1], c = input[2]
+//    if !input.filter({ $0 <= 0 }).isEmpty {
+//        print("w(\(a), \(b), \(c)) = 1")
+//        continue
+//    }
+//    if !input.filter({ $0 > 20}).isEmpty {
+//        print("w(\(a), \(b), \(c)) = \(w[20][20][20])")
+//        continue
+//    }
+//    print("w(\(a), \(b), \(c)) = \(w[a][b][c])")
+//}
--- BaekJoon/main.swift
+++ BaekJoon/main.swift
@@ -4,24 +4,29 @@
 //
 //  Created by 양시관 on 1/19/24.
 import Foundation
-func solution() -> Int {
-    let n = Int(readLine()!)!
-    var first: Int = 1
-    var second: Int = 2
-    if n == 1 {
-        return first
-    } else if n == 2 {
-        return second
-    } else {
-        var result: Int = 0
-        for _ in 3...n{
-            result = (first + second) % 15746
-            first = second % 15746
-            second = result
+var w = [[[Int]]](repeating: [[Int]](repeating: [Int](repeating: 1, count: 21), count: 21), count: 21)
+
+for a in 1...20 {
+    for b in 1...20 {
+        for c in 1...20 {
+            if a < b && b < c {
+                w[a][b][c] = w[a][b][c - 1] + w[a][b - 1][c - 1] - w[a][b - 1][c]
+            } else {
+                w[a][b][c] = w[a - 1][b][c] + w[a - 1][b - 1][c] + w[a - 1][b][c - 1] - w[a - 1][b - 1][c - 1]
+            }
         }
-        return result
     }
 }
 
-print(solution())
-
+while let input = readLine()?.split(separator: " ").map({ Int($0)! }), input != [-1 ,-1, -1] {
+    let a = input[0], b = input[1], c = input[2]
+    if !input.filter({ $0 <= 0 }).isEmpty {
+        print("w(\(a), \(b), \(c)) = 1")
+        continue
+    }
+    if !input.filter({ $0 > 20}).isEmpty {
+        print("w(\(a), \(b), \(c)) = \(w[20][20][20])")
+        continue
+    }
+    print("w(\(a), \(b), \(c)) = \(w[a][b][c])")
+}
