5711d5738e8c348eedf28f3d0aeaad2f358188ba
sikwan <yysskk99999@naver.com>
2024-02-23 15:09:21 +0900

    25682

--- BaekJoon/main.swift
+++ BaekJoon/main.swift
@@ -4,26 +4,41 @@
 //
 //  Created by 양시관 on 1/19/24.
 import Foundation
-let nm = readLine()!.split(separator: " ").compactMap { Int($0) }
-let n = nm[0]
-let m = nm[1]
-var matrix: [[Int]] = [Array(repeating: 0, count: n+1)]
+let input = readLine()!.split(separator: " ").map { Int($0)! }
+let n = input[0], m = input[1], k = input[2]
+var board: [[Character]] = []
 
 for _ in 0..<n {
-    let input = readLine()!.split(separator: " ").compactMap { Int($0) }
-    matrix.append([0] + input)
+    board.append(readLine()!.map { $0 })
 }
 
-var dp = matrix
-for i in 1...n {
-    for j in 1...n {
-        dp[i][j] = matrix[i][j] + dp[i-1][j] + dp[i][j-1] - dp[i-1][j-1]
+
+func makePrefixSum(color: Character) -> [[Int]] {
+    var prefixSum = [[Int]](repeating: [Int](repeating: 0, count: m + 1), count: n + 1)
+    var equalValue = 0
+    for y in 0..<n {
+        for x in 0..<m {
+            if (y + x) % 2 == 0 {
+                equalValue = board[y][x] == color ? 0 : 1
+            } else {
+                equalValue = board[y][x] != color ? 0 : 1
+            }
+            prefixSum[y + 1][x + 1] = prefixSum[y][x + 1] + prefixSum[y + 1][x] - prefixSum[y][x] + equalValue
+        }
     }
+    return prefixSum
 }
 
-for _ in 0..<m {
-    let input = readLine()!.split(separator: " ").compactMap { Int($0) }
-    let (x1,y1,x2,y2) = (input[0], input[1], input[2], input[3])
-    print(dp[x2][y2] - dp[x1-1][y2] - dp[x2][y1-1] + dp[x1-1][y1-1])
+func squareMinCount(_ prefixSum: [[Int]]) -> Int {
+    var answer = Int.max
+    for y in 1...(n - k + 1) {
+        for x in 1...(m - k + 1) {
+            let count = prefixSum[y + k - 1][x + k - 1] - prefixSum[y - 1][x + k - 1] - prefixSum[y + k - 1][x - 1] + prefixSum[y - 1][x - 1]
+            answer = min(answer, count)
+        }
+    }
+    return answer
 }
+
+print(min(squareMinCount(makePrefixSum(color: "B")), squareMinCount(makePrefixSum(color: "W"))))
 
