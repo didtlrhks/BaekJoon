c413b26ff26d6fa4f8cb20a4485bb0046fa4651d
sikwan <yysskk99999@naver.com>
2024-02-25 20:51:27 +0900

    1427

---
+++ BaekJoon/1427.swift
@@ -0,0 +1,19 @@
+////
+////  1427.swift
+////  BaekJoon
+////
+////  Created by 양시관 on 2/25/24.
+////
+//
+//import Foundation
+//let n = readLine()!
+//var arr: [Int] = []
+//
+//for i in n {
+//    arr.append(Int(String(i))!)
+//}
+//arr.sort(by: >)
+//
+//for k in 0..<arr.count {
+//    print("\(arr[k])", terminator: "")
+//}
--- BaekJoon/main.swift
+++ BaekJoon/main.swift
@@ -3,42 +3,14 @@
 //  BaekJoon
 //
 //  Created by 양시관 on 1/19/24.
-import Foundation
-let input = readLine()!.split(separator: " ").map { Int($0)! }
-let n = input[0], m = input[1], k = input[2]
-var board: [[Character]] = []
+let n = readLine()!
+var arr: [Int] = []
 
-for _ in 0..<n {
-    board.append(readLine()!.map { $0 })
+for i in n {
+    arr.append(Int(String(i))!)
 }
+arr.sort(by: >)
 
-
-func makePrefixSum(color: Character) -> [[Int]] {
-    var prefixSum = [[Int]](repeating: [Int](repeating: 0, count: m + 1), count: n + 1)
-    var equalValue = 0
-    for y in 0..<n {
-        for x in 0..<m {
-            if (y + x) % 2 == 0 {
-                equalValue = board[y][x] == color ? 0 : 1
-            } else {
-                equalValue = board[y][x] != color ? 0 : 1
-            }
-            prefixSum[y + 1][x + 1] = prefixSum[y][x + 1] + prefixSum[y + 1][x] - prefixSum[y][x] + equalValue
-        }
-    }
-    return prefixSum
+for k in 0..<arr.count {
+    print("\(arr[k])", terminator: "")
 }
-
-func squareMinCount(_ prefixSum: [[Int]]) -> Int {
-    var answer = Int.max
-    for y in 1...(n - k + 1) {
-        for x in 1...(m - k + 1) {
-            let count = prefixSum[y + k - 1][x + k - 1] - prefixSum[y - 1][x + k - 1] - prefixSum[y + k - 1][x - 1] + prefixSum[y - 1][x - 1]
-            answer = min(answer, count)
-        }
-    }
-    return answer
-}
-
-print(min(squareMinCount(makePrefixSum(color: "B")), squareMinCount(makePrefixSum(color: "W"))))
-
