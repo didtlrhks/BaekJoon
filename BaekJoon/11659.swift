00e82d8f492ad19bc83fc4488758e0086cfac603
sikwan <yysskk99999@naver.com>
2024-02-26 22:52:54 +0900

    11659

---
+++ BaekJoon/11659.swift
@@ -0,0 +1,20 @@
+////
+////  11659.swift
+////  BaekJoon
+////
+////  Created by 양시관 on 2/26/24.
+////
+//
+//import Foundation
+//let input = readLine()!.split(separator: " ").map { Int($0)! }
+//let n = input[0], m = input[1]
+//var array = [0] + readLine()!.split(separator: " ").map { Int($0)! }
+//for i in 1...n {
+//    array[i] += array[i - 1]
+//}
+//
+//for _ in 0..<m {
+//    let input = readLine()!.split(separator: " ").map { Int($0)! }
+//    let i = input[0], j = input[1]
+//    print(array[j] - array[i - 1])
+//}
--- BaekJoon/main.swift
+++ BaekJoon/main.swift
@@ -3,14 +3,15 @@
 //  BaekJoon
 //
 //  Created by 양시관 on 1/19/24.
-let n = readLine()!
-var arr: [Int] = []
-
-for i in n {
-    arr.append(Int(String(i))!)
+let input = readLine()!.split(separator: " ").map { Int($0)! }
+let n = input[0], m = input[1]
+var array = [0] + readLine()!.split(separator: " ").map { Int($0)! }
+for i in 1...n {
+    array[i] += array[i - 1]
 }
-arr.sort(by: >)
 
-for k in 0..<arr.count {
-    print("\(arr[k])", terminator: "")
+for _ in 0..<m {
+    let input = readLine()!.split(separator: " ").map { Int($0)! }
+    let i = input[0], j = input[1]
+    print(array[j] - array[i - 1])
 }
